d2e675b6c288911f20e30e04372962d2c42f92e3
Jesse Brinkley <jbrink28@gmail.com>
2020-08-21 03:13:32 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,12 +7,3 @@ def countdown_with_sleep(num)
   end
   "HAPPY NEW YEAR!"
 end
-
-def countdown_with_sleep(num)
-  while num >= 1
-  puts "#{num} SECOND(S)!"
-  sleep(1)
-  num -= 1
-  end
-  "HAPPY NEW YEAR!"
-end
